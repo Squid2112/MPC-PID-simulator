00b27e21db209afbd4d4784962c909be6db9e369
Squid2112 <squid@thixo.com>
2025-02-18 23:37:48 -0600

    fix to correct problems with VS2022

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/github/MPC-PID_simulator
+# Install script for directory: D:/github/MPC-PID-simulator
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "D:/github/MPC-PID_simulator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "D:/github/MPC-PID-simulator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- simulation.h
+++ simulation.h
@@ -7,23 +7,25 @@
 #include "hybrid_controller.h"
 #include <vector>
 
+constexpr double PI = 3.14159265358979323846;
+
 class Simulation {
 public:
-    Simulation(double initial_T);
-    void run();
-    void print_results() const;
+	Simulation(double initial_T);
+	void run();
+	void print_results() const;
 
 private:
-    ThermalSystem thermal_system;
-    PIDController pid_controller;
-    MPCController mpc_controller;
-    HybridController hybrid_controller;
-    std::vector<double> times;
-    std::vector<double> temperatures;
-    std::vector<double> ambient_temps;
-    std::vector<double> control_inputs;
+	ThermalSystem thermal_system;
+	PIDController pid_controller;
+	MPCController mpc_controller;
+	HybridController hybrid_controller;
+	std::vector<double> times;
+	std::vector<double> temperatures;
+	std::vector<double> ambient_temps;
+	std::vector<double> control_inputs;
 
-    double T_amb(double t) const;
+	double T_amb(double t) const;
 };
 
 #endif // SIMULATION_H
